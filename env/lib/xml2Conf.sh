b7681cb7910cfef6b57b36eb5b4aafb407db6167
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-09-23 23:18:26 +0530

    👌 IMPROVE: First commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/wired/Documents/ml-project/env/lib"
+XML2_LIBS="-lxml2 -L/Users/wired/Documents/ml-project/env/lib -lz -L/Users/wired/Documents/ml-project/env/lib -llzma -lpthread -L/Users/wired/Documents/ml-project/env/lib -liconv -L/Users/wired/Documents/ml-project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/wired/Documents/ml-project/env/include/libxml2 -I/Users/wired/Documents/ml-project/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
