2ad75a5c7d891cd013f59f5d09196af8ef81a929
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-09-24 10:09:14 +0530

    Add yml file

--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/wired/Documents/ml-project/env/lib"
-XSLT_LIBS="-lxslt -L/Users/wired/Documents/ml-project/env/lib -lxml2 -L/Users/wired/Documents/ml-project/env/lib -L/Users/wired/Documents/ml-project/env/lib -lpthread -L/Users/wired/Documents/ml-project/env/lib -liconv -L/Users/wired/Documents/ml-project/env/lib -licui18n -licuuc -licudata -lm "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/wired/Documents/ml-project/env/include"
-MODULE_VERSION="xslt-1.1.35"
